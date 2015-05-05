b3f36322a94046ecd318b7c553f72d7f411da0a0
Mr_Faton <firefly90@inbox.ru>
2015-05-05 08:07:46 +0300

    update from work

--- src/com/mr_faton/handler/HandleButtonHandler.java
+++ src/com/mr_faton/handler/HandleButtonHandler.java
@@ -11,9 +11,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by Mr_Faton on 04.05.2015.
@@ -25,11 +23,14 @@ public final class HandleButtonHandler {
     private BasicFileAttributes fileAttributes;
     private String sourceDir;
     private String destDir;
+    private String[] months;
 
     public HandleButtonHandler() {
         centerPanel = CenterPanel.getInstance();
         southPanel = SouthPanel.getInstance();
         fileList = new LinkedList<File>();
+        months = new String[]{"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь",
+                "Октябрь", "Ноябрь", "Декабрь"};
     }
 
     public void handle() {
@@ -38,7 +39,7 @@ public final class HandleButtonHandler {
             southPanel.setProgressBarVisible(true);
             collectAllFiles(new File(sourceDir));
             for (File file : fileList) {
-                String finalDirPath = getFinalDirPath(file);
+                Map<String, String> fileNameAndFileDirMap = getFileNameAndFileDir(file);
             }
 
         } catch (IllegalArgumentException ex) {
@@ -48,7 +49,7 @@ public final class HandleButtonHandler {
     }
 
 
-    private void saveDirs() throws IllegalArgumentException{
+    private void saveDirs() throws IllegalArgumentException {
         sourceDir = centerPanel.getInputDir();
         if (sourceDir == null || sourceDir.length() <= 1) {
             showErrorMessage("Поле папки-источкика незаполнено!\nВыбери папку-источник.");
@@ -82,24 +83,70 @@ public final class HandleButtonHandler {
         }
     }
 
-    private String getFinalDirPath(File file) {
+    private Map<String, String> getFileNameAndFileDir(File file) {
+        StringBuilder pathToFile;
+        Calendar lastModifiedDate;
+        Map<String, String> fileNameAndFileDirMap;
+        String day;
+        String month;
+        String year;
+        String hour;
+        String minute;
+        String second;
+        String fileDir;
+        String fileName;
+
         try {
             fileAttributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
         } catch (IOException e) {
             ///1111111111111111111111111111
             e.printStackTrace();
         }
-        StringBuilder pathToFile = new StringBuilder();
+        pathToFile = new StringBuilder();
         pathToFile.append(sourceDir + "\\");
-        fileAttributes.fileKey();
+
+        lastModifiedDate = new GregorianCalendar();
+        lastModifiedDate.setTimeInMillis(fileAttributes.lastModifiedTime().toMillis());
+        day = lastModifiedDate.get(Calendar.DAY_OF_MONTH) + "";
+        month = lastModifiedDate.get(Calendar.MONTH) + "";
+        year = lastModifiedDate.get(Calendar.YEAR) + "";
+        hour = lastModifiedDate.get(Calendar.HOUR) + "";
+        minute = lastModifiedDate.get(Calendar.MINUTE) + "";
+        second = lastModifiedDate.get(Calendar.SECOND) + "";
+
+
         return null;
     }
 
-    private void copyFile(File source, File dest) throws IOException{
+    private void copyFile(File source, File dest) throws IOException {
         Files.copy(source.toPath(), dest.toPath());
     }
 
     private void showErrorMessage(String message) {
         JOptionPane.showMessageDialog(StartProgram.mainFrame, message, "Ошибка!", JOptionPane.ERROR_MESSAGE);
+    }
+
+    private String getFileExtension(File file) {
+        String name = file.getName();
+        int position = name.lastIndexOf('.');
+        if (position > 0) {
+            return name.substring(position + 1);
+        }
+        return "";
+    }
+
+    private String getMonthDir(String monthStr) {
+        if (monthStr != null && monthStr.length() != 0) {
+            int month = Integer.valueOf(monthStr);
+            ////
+            return "";
+        } else {
+            return "";
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
     }
 }
--- src/com/mr_faton/test/Tt.java
+++ src/com/mr_faton/test/Tt.java
@@ -1,23 +1,23 @@
 package com.mr_faton.test;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Map;
+
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by root on 04.05.2015.
  */
 public class Tt {
-    public static void main(String[] args) throws Exception{
-//        File file = new File("C:\\qq\\ww.jpg");
-        File file = new File("C:\\qq\\ee.mp4");
-        Map<String, Object> stringObjectMap = Files.readAttributes(file.toPath(), "*");
-        for (Map.Entry<String, Object> entry : stringObjectMap.entrySet()) {
-            System.out.println(entry.getKey() + "=" + entry.getValue());
-        }
+    public static void main(String[] args) {
+        String[] months = new DateFormatSymbols().getMonths();
+        System.out.println(Arrays.toString(months));
+        Calendar mCalendar = Calendar.getInstance();
+        String month = mCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        System.out.println(month);
+        String str = String.format("%tB", mCalendar);
+        System.out.println(str);
     }
 }
-/*
-lastModifiedTime=2014-12-07T16:41:55Z
- */
